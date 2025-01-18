6b4ee961da735640656682dfbf58fd08f1cb52e8
Braxton Salyer <braxtonsalyer@gmail.com>
2025-01-18 14:02:08 -0600

    Add hyperion_assert version 0.2.0

--- packages/h/hyperion_assert/xmake.lua
+++ packages/h/hyperion_assert/xmake.lua
@@ -3,7 +3,10 @@ package("hyperion_assert")
 set_homepage("https://braxtons12.github.io/hyperion_mpl/")
 set_description("C++20 Runtime Assertions Library")
 set_license("MIT")
-add_urls("https://github.com/braxtons12/hyperion_assert.git")
+add_urls("https://github.com/braxtons12/hyperion_assert/archive/refs/tags/v$(version).tar.gz",
+    "https://github.com/braxtons12/hyperion_assert.git")
+add_versions("0.1.0", "ac37fe69e1b0bcf9e8838058d7f551e20b19c8bb35ecce4b0cbc1b28e1b8fe5c")
+add_versions("0.2.0", "f64acfc2a8cc071a496df1c4a2f42141d34e27498f9c6d80a9d3216ae53240a6")
 
 add_configs("hyperion_enable_tracy",
     { description = "Enable Tracy profiling", default = false, values = { false, true } })
@@ -52,7 +55,7 @@ on_load(function(package)
     })
 
     if package:config("hyperion_enable_tracy") then
-        package:add("defines", "TRACY_ENABLE=1", {public = true})
+        package:add("defines", "TRACY_ENABLE=1", { public = true })
     end
 end)
 
